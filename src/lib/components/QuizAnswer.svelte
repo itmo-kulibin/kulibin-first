88ea331bd3a570de8aaeb041df297b079bc279bd
Alexei Kuzmichev <alexeikuzmichev17@gmail.com>
2025-04-01 23:40:59 +0300

    Some fixes to Quizes

--- src/lib/components/QuizAnswer.svelte
+++ src/lib/components/QuizAnswer.svelte
@@ -12,7 +12,6 @@
     }
 </script>
 
-
 <button class="QuizButton text-lg {buttonState == 0 ? '' : buttonState == 1 ? 'correct' : 'incorrect'} {active ? '' : 'inactive'}" onclick={checkTrue} >
     {answer}
 </button>
@@ -28,20 +27,35 @@
             color: white;
         }
     }
+
     .QuizButton {
         display: flex;
         align-items: center;
         justify-content: center;
         width: min(172.5px, calc(((100vw - 76px) / 2)));
         min-height: 60px;
-        /* height: 100%; */
-        /* height: 40px; */
+        font-size: 18px;
         gap: 8px;
         border-radius: 100px;
         border-width: 1px;
         color: #344CB7;
         border: 1px solid #344CB7
     }
+
+    @media screen and (max-width: 340px) {
+        .QuizButton {
+            font-size: 16px;
+            min-height: 40px;
+        }
+    }
+
+    @media screen and (max-width: 318px) {
+        .QuizButton {
+            font-size: 12px;
+            min-height: 40px;
+        }
+    }
+
     .QuizButton:hover {
         border: 1px solid #000957
     }
