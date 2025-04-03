0de1d2960f415223c97a0d5f64da6cf348e3d90a
ksvluc <kd.svechnikov@gmail.com>
2025-04-03 16:15:35 +0300

    Картинку пока вставил свою, все остальное сделал

---
+++ src/routes/__components-demo/Boat/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+    import Boat from '$lib/components/Boat.svelte';
+</script>
+
+<main class="main-content">
+    <Boat />
+</main>
+
+<style>
+    .main-content {
+        margin: 0 auto;
+        text-align: center;
+        background: #FFF2F2;
+        color: #000000;
+    }
+
+</style>
