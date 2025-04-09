b64ffca13b65f0854edd9a0f982e58a1d606f8ca
SemyonHoyrish <semyonhoyrish@gmail.com>
2025-04-09 23:39:48 +0300

    small fix

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -96,7 +96,7 @@
         class="header" 
         class:header-hidden={!isHeaderVisible}
     >
-        <a class="title" href="{base}/">Кулибин</a>
+    <a class="title" href="{base}/" on:click={() => {menuExtended = false}}>Кулибин</a>
         <button on:click={() => {menuExtended = !menuExtended;}} class="hamburger-button" aria-label="Открыть меню">
             <svg width="60" height="60" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M5 30V26.6667H35V30H5ZM5 21.6667V18.3333H35V21.6667H5ZM5 13.3333V10H35V13.3333H5Z" fill="#FFF2F2"/>
